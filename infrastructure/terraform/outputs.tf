87aac45881f82e3547f4176745b6b491b8d1ddf1
dwezis <emax151004@gmail.com>
2026-05-12 22:19:13 +0300

    Emeshkin_heat

--- infrastructure/terraform/outputs.tf
+++ infrastructure/terraform/outputs.tf
@@ -1,12 +1,20 @@
 output "server_ip" {
   description = "VM fixed IP (existing or newly created)"
-  value = trimspace(var.existing_server_id) != ""
-    ? (trimspace(var.existing_server_ip) != "" ? var.existing_server_ip : data.openstack_compute_instance_v2.existing_server[0].access_ip_v4)
-    : openstack_compute_instance_v2.app_server[0].access_ip_v4
+  value = (
+    trimspace(var.existing_server_id) != "" ? (
+      trimspace(var.existing_server_ip) != "" ?
+      var.existing_server_ip :
+      data.openstack_compute_instance_v2.existing_server[0].access_ip_v4
+    ) : openstack_compute_instance_v2.app_server[0].access_ip_v4
+  )
 }
 
 output "server_name" {
-  value = trimspace(var.existing_server_id) != "" ? data.openstack_compute_instance_v2.existing_server[0].name : openstack_compute_instance_v2.app_server[0].name
+  value = (
+    trimspace(var.existing_server_id) != "" ?
+    data.openstack_compute_instance_v2.existing_server[0].name :
+    openstack_compute_instance_v2.app_server[0].name
+  )
 }
 
 output "postgres_volume_id" {
@@ -15,7 +23,9 @@ output "postgres_volume_id" {
 }
 
 output "ssh_command" {
-  value = trimspace(var.existing_server_id) != ""
-    ? "ssh ubuntu@${trimspace(var.existing_server_ip) != "" ? var.existing_server_ip : data.openstack_compute_instance_v2.existing_server[0].access_ip_v4}"
-    : "ssh ubuntu@${openstack_compute_instance_v2.app_server[0].access_ip_v4}"
+  value = (
+    trimspace(var.existing_server_id) != "" ?
+    "ssh ubuntu@${trimspace(var.existing_server_ip) != "" ? var.existing_server_ip : data.openstack_compute_instance_v2.existing_server[0].access_ip_v4}" :
+    "ssh ubuntu@${openstack_compute_instance_v2.app_server[0].access_ip_v4}"
+  )
 }
